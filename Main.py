b3d1b7c2c9bb75109d5ae4d5e443728694119218
GameCobra <adrian@houser.ca>
2024-08-22 22:36:31 -0400

    Testing for multilayer redirects

--- Main.py
+++ Main.py
@@ -1,12 +1,30 @@
 from bs4 import BeautifulSoup
 import requests 
+from requests_html import HTMLSession
 
 
-URL = "https://www.reddit.com/r/BehindTheTables/wiki/index/"
-page = requests.get(URL)
+# Everything broken
+# Get the redirect of the url found on the redit page
+# remove the rdt text [:-10]
+# then append the url link text name (hyper link)
+# use that as the redit page to look at
+
+
+
+URL = "https://redd.it/4iq4of"
+
+page = requests.get(URL, allow_redirects=True)
+page = requests.get(page.url[:-10], allow_redirects=True)
+print(page.url)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
+
+data = []
+for link in soup.find_all("a"):
+    data.append(link.get('href'))
+
 f = open("demofile3.txt", "w")
-f.write(page.text)
+for item in data:
+    f.write(item + "\n")
 f.close()
