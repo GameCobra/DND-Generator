8369389664c94f305b4badfd2973004a6866d91d
GameCobra <adrian@houser.ca>
2024-09-04 20:12:43 -0400

    Repeated calling?

--- Display.py
+++ Display.py
@@ -12,11 +12,33 @@ def mainDictToList():
         newList.append(i["Name"])
     return newList
 
+def isInputNumber(inputVal : str, reCall : bool = False, toCall = None):
+    try:
+        inputVal = int(inputVal)
+    except:
+        if reCall == True:
+            f = toCall()
+            return isInputNumber(f, reCall, toCall)
+        else:
+            return None
+    else:
+        return None
+        
+
 def displaySelectionList(itemeList: list, menuName: str):
     print(f"-----{menuName}-----")
     for i in range(len(itemeList)):
         print(f"{i} - {itemeList[i]}")
     return input("> ")
 
- 
-displaySelectionList(mainDictToList(), "main")
+def mainMenu():
+    displaySelectionList(["Random thing generation"])
+
+#mainMenu()
+
+def randomThingGenerator():
+    isInputNumber(displaySelectionList(mainDictToList(), "random"))
+
+randomThingGenerator()
+
+#displaySelectionList(mainDictToList(), "main")
