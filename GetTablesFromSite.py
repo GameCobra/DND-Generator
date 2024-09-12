fc37f866060a761c803a68ca00477ac4ac629b16
GameCobra <adrian@houser.ca>
2024-09-11 21:40:06 -0400

    Removed uneccesary components of Tables

--- GetTablesFromSite.py
+++ GetTablesFromSite.py
@@ -20,6 +20,8 @@ def firstTable(inputText : str):
     firstValue = inputText.find("<strong>d")
     text = text[firstValue:]
     secondValue = text.find("</ol>")
+    if secondValue == -1:
+        secondValue = text.find("</div>")
     #print(secondValue)
     text = text[:secondValue]
     return text, secondValue + firstValue
@@ -28,10 +30,19 @@ def getTables(inputText : str, depth : int):
     result = firstTable(inputText)
     if result[0] == "":
         return
-    vals[len(vals) - 1]['Table'].append(result[0])
+    addTo(result[0])
     inputText = inputText[result[1]:]
     if depth > 0:
         getTables(inputText, depth - 1)
+
+def addTo(value : str):
+    removeVals = ["</li><li>\n", "</p>\n", "<p>\n", "<strong>", "</strong>", "<li>\n", "</p><ol>\n", "</li>"]
+    changeForEnter = ["<br />"]
+    for i in range(len(removeVals)):
+        value = value.replace(removeVals[i], "")
+    for i in range(len(changeForEnter)):
+        value = value.replace(changeForEnter[i], "")
+    vals[len(vals) - 1]['Table'].append(value)
 
 def fullTable(inputText : str, inputName : str):
     vals.append({"Name": inputName, "Table" : []})
@@ -49,7 +60,7 @@ for i in range(5):
     print(i)
     page = requests.get(JSONList[i]["URL"], allow_redirects=True)
     fullTable(page.text, JSONList[i]["Name"])
-    time.sleep(2.5)
+    #time.sleep(1)
 
 with open("Tables.json", "w") as f:
     json.dump(vals, f)
