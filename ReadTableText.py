09e3878d390cd4fe64a888d95fba5bf0d82d40d9
GameCobra <adrian@houser.ca>
2024-09-14 20:43:37 -0400

    Removed line breaks from tables in table reader

--- ReadTableText.py
+++ ReadTableText.py
@@ -2,4 +2,9 @@ import json
 
 file = open('RedditTables\ReditPage copy 1', 'r')
 Lines = file.readlines()
-print(Lines)
+textLines : list = []
+for i in range(len(Lines)):
+    textLines.append(Lines[i].replace("\n", ""))
+    if len(textLines[len(textLines) - 1]) == 0:
+        textLines.pop(len(textLines) - 1)
+print(textLines)
