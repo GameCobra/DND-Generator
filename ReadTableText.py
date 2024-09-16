20fdbf4f9217db73a5f720ffda423fb3654c9b6a
GameCobra <adrian@houser.ca>
2024-09-15 21:12:05 -0400

    Getting values from table

--- ReadTableText.py
+++ ReadTableText.py
@@ -1,5 +1,7 @@
 import json
 
+tables = []
+
 file = open('RedditTables\ReditPage copy 1', 'r')
 Lines = file.readlines()
 textLines : list = []
@@ -7,4 +9,15 @@ for i in range(len(Lines)):
     textLines.append(Lines[i].replace("\n", ""))
     if len(textLines[len(textLines) - 1]) == 0:
         textLines.pop(len(textLines) - 1)
+pageName = textLines[0]
+textLines.pop(0)
+diceValue = 0
+try:
+    diceValue = int(textLines[0][1:4])
+except:
+    diceValue = int(textLines[0][1:3])
+tables.append({"Title" : pageName, "Dice Value": diceValue, "Entries" : textLines[1:diceValue + 1]})
+textLines = textLines[diceValue + 1:]
+print(tables)
 print(textLines)
+#print(textLines)
