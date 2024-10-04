7ddb7d132c9e66a363d9b891db129c17b6164aff
GameCobra <adrian@houser.ca>
2024-10-04 13:46:03 -0400

    All display layer working

--- Display.py
+++ Display.py
@@ -1,5 +1,6 @@
 import json
 import time
+import random
 
 Tables = []
 
@@ -21,18 +22,41 @@ def GrabSubSections(index : int):
         newList.append(i["Sub Title"])
     return newList
 
+def GrabSubSubSections(index : int, subIndex : int):
+    l = GrabHeaders()
+    newList = []
+    for i in Tables[index]["Sub Pages"][subIndex]["Table"]:
+        #print(i)
+        dice = random.randint(0, int(i["Amount"]))
+        selEntry = "No Entrie"
+        for j in i["Entries"]:
+            if int(j["Min"]) <= dice and int(j["Max"]) >= dice:
+                selEntry = j["Text"]
+                break
+        newList.append(i["Table Title"] + "  " + selEntry)
+    return newList
+
 
 #* Wanted commands
 #* Sel, Main, Ser
+
+def search(arg : str, layer):
+    print(arg)
+    exit()
 
 def isInputNumber(inputVal : str, callFunction):
     try:
         inputVal = int(inputVal)
     except:
-        print("Plese enter a propper value")
-        time.sleep(1)
-        callFunction()
-        return None
+        if inputVal == "menu":
+            mainMenu()
+        elif inputVal.startswith("ser "):
+            search(inputVal[4:], callFunction)
+        else:
+            print("Plese enter a propper value")
+            time.sleep(1)
+            callFunction()
+            return None
     else:
         return inputVal
         
@@ -58,19 +82,27 @@ def CompiledDisplay(itemList: list, menuName: str, returnFunction):
         returnFunction()
     return value
 
-def randomThingGenerator():
-    result = CompiledDisplay(GrabHeaders(), "random", randomThingGenerator)
-    CompiledDisplay(GrabSubSections(result), "sub", randomThingGenerator)
+def TopLayer():
+    result = CompiledDisplay(GrabHeaders(), "random", TopLayer)
+    SecondLayer(result)
     #if result == None:
     #    print("Plese enter a propper value")
     #    time.sleep(1)
     #    randomThingGenerator()
 
+def SecondLayer(result):
+    result2 = CompiledDisplay(GrabSubSections(result - 1), "sub", SecondLayer)
+    TheirdLayer(result, result2)
+
+def TheirdLayer(result, result2):
+    CompiledDisplay(GrabSubSubSections(result - 1, result2 - 1), "sub sub", TheirdLayer)
+
+
 def mainMenu():
     while True:
         value = CompiledDisplay(["Random thing generation", "Settings", "Help", "Credits"], "Menu", mainMenu)
         if value == 1:
-            randomThingGenerator()
+            TopLayer()
         if value == 3:
             print("Please check the .readme file for instructions")
             time.sleep(1)
